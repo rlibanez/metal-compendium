e11c8c9ce0a097e4d24543a2556b954ef54f7363
rlibanez <rlibanezz@gmail.com>
2024-05-29 23:52:52 +0200

    New Java project with Spring Boot

---
+++ metal-compendium/src/main/java/com/rlibanez/metalcompendium/MetalCompendiumApplication.java
@@ -0,0 +1,13 @@
+package com.rlibanez.metalcompendium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetalCompendiumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetalCompendiumApplication.class, args);
+	}
+
+}
---
+++ metal-compendium/src/test/java/com/rlibanez/metalcompendium/MetalCompendiumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rlibanez.metalcompendium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetalCompendiumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
