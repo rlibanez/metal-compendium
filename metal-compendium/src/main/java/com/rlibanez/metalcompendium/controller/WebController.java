d8b6aaa452011fe06d000794e6a5c8a5ec3869fd
rlibanez <rlibanezz@gmail.com>
2024-05-31 21:01:47 +0200

    Add basic Thymeleaf index.html

---
+++ metal-compendium/src/main/java/com/rlibanez/metalcompendium/controller/WebController.java
@@ -0,0 +1,17 @@
+package com.rlibanez.metalcompendium.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class WebController {
+
+	@GetMapping("/")
+	public String welcome(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
+			Model model) {
+		model.addAttribute("name", name);
+		return "index";
+	}
+}
